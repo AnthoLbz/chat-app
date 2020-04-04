40df02fe83069b2da507c8e25802fe3b4101fdbc
Anthony Lasbleiz <anthony.lasbleiz@gmail.com>
2020-04-04 16:43:23 +0200

    finished 3rd react quest

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import './App.css';
-import Contact from './components/contact';
-
-const Darlene = {
-  name: "Darlene Thomas",
-  avatar: "https://randomuser.me/api/portraits/women/22.jpg",
-  online: true
-};
-
-const Warren = {
-  name: "Warren Lambert",
-  avatar: "https://randomuser.me/api/portraits/men/72.jpg",
-  online: false
-};
-
-const Bryan = {
-  name: "Bryan Hopkins",
-  avatar: "https://randomuser.me/api/portraits/men/26.jpg",
-  online: true
-};
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div>
-      <Contact {...Darlene} />
-      <Contact {...Warren} />
-      <Contact {...Bryan} />
+      <ContactList />
     </div>
   );
 }
